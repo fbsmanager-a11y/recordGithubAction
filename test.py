84880aa40117a5aa776656611e7cb2f34027e328
fbsmanager-a11y <fbsmanager@gmail.com>
2025-10-01 15:16:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -10,4 +10,4 @@ soup = BeautifulSoup(html, 'html.parser')
 #class="fig-w1hthz e7z8ge71"
 bookservices = soup.select('.fig-w1hthz')
 for no, book in enumerate(bookservices, 1):
-    print(no, book.text.strip())
+    print(no, book.text.strip()) 
