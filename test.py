47dbe03180000030df3c8c602829f302c7fad85e
fbsmanager-a11y <fbsmanager@gmail.com>
2025-10-01 16:12:01 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 # url = 'https://search.naver.com/search.naver?where=nexearch&sm=tab_etc&mra=bjky&pkid=1&os=28410101&qvt=0&query=%EA%B0%95%ED%98%95%EC%B2%A0' 
-url = 'https://ridibooks.com/category/new-releases/2200'
+url = 'https://ridibooks.com/category/new-releases/2200' 
 response = requests.get(url)
 response.encoding = 'utf-8'
 html = response.text
